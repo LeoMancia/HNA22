b3871916836f563507583b544112ea0149286f8f
mzkovga <koz147852@gmail.com>
2022-02-22 11:43:48 -0600

    2022-02-22
    1° commit to main from oscar

---
+++ insumos/resources/views/crud/create.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Title dash</h1>
+@stop
+
+@section('content')
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
